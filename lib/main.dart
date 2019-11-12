88b221b7e73bd18180767cdbb3175e5460827d32
Abraxius <josti4a@t-online.de>
2019-11-12 15:13:35 +0100

    Alte UI implementiert

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Szenes/Startseite.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,10 +10,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Startseite(),
     );
   }
 }
